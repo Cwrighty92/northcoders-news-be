1b8d8b642381be8556121810c0d11e269038786b
Chris Wright <chriswrighty1192@gmail.com>
2018-07-07 10:32:06 +0100

    s

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyparser = require("body-parser");
 const apiRouter = require("./routes/api");
 const mongoose = require("mongoose");
 const DB_URL = process.env.DB_URL || require("./config").DB_URL;
-const { handle400 } = require("./errorHandlers/error");
+const handleErrors = require("./errorHandlers/error");
 app.use(bodyparser.json());
 app.set("view engine", "ejs");
 
@@ -13,47 +13,16 @@ mongoose.connect(
 );
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.status(200).send("All good");
+  res.status(200).send("Add API onto the URL link to get to the right page");
 });
 app.use("/api", apiRouter);
 
+app.use(handleErrors);
+
 app.use((err, req, res, next) => {
-  if (
-    err.message ===
-    "Either there are no comments in this article or the article id is invalid"
-  ) {
-    res.status(404).send({
-      message:
-        "Either there are no comments in this article or the article id is invalid"
-    });
-  }
-  if (
-    err.message === "Bad request: Only takes up or down query to make a vote "
-  ) {
-    res.status(400).send({
-      message: "Bad request: Only takes up or down query to make a vote"
-    });
-  }
-  if (err.message === "No user found with given userName") {
-    res.status(404).send({
-      message: "Page not found with given userName, it does not exist"
-    });
-  }
-  if (err.message === "Page not found") {
-    res.status(404).send({ message: "Page not found for given Id" });
-  }
-  if (err.name === "CastError") {
-    res.status(400).send({
-      message: `${
-        err.value
-      } is in an incorrect format or does not exist Check the ReadMe`
-    });
-  }
-  if (err.name === "ValidationError") {
-    res.status(400).send({
-      message: `The object provided is not in the correct format please see the readMe for guidance`
-    });
-  }
+  res
+    .status(500)
+    .send({ message: "Server Error, Apologies for the inconvinience" });
 });
 
 module.exports = app;
--- errorHandlers/error.js
+++ errorHandlers/error.js
@@ -1,5 +1,40 @@
-const handle400 = (err, req, res, next) => {
-  console.log(err);
+const handleErrors = (err, req, res, next) => {
+  if (
+    err.message ===
+    "Either there are no comments in this article or the article id is invalid"
+  ) {
+    res.status(404).send({
+      message:
+        "Either there are no comments in this article or the article id is invalid"
+    });
+  }
+  if (
+    err.message === "Bad request: Only takes up or down query to make a vote "
+  ) {
+    res.status(400).send({
+      message: "Bad request: Only takes up or down query to make a vote"
+    });
+  }
+  if (err.message === "No user found with given userName") {
+    res.status(404).send({
+      message: "Page not found with given userName, it does not exist"
+    });
+  }
+  if (err.message === "Page not found") {
+    res.status(404).send({ message: "Page not found for given Id" });
+  }
+  if (err.name === "CastError") {
+    res.status(400).send({
+      message: `${
+        err.value
+      } is in an incorrect format or does not exist Check the ReadMe`
+    });
+  }
+  if (err.name === "ValidationError") {
+    res.status(400).send({
+      message: `The object provided is not in the correct format please see the readMe for guidance`
+    });
+  }
 };
 
-module.exports = { handle400 };
+module.exports = handleErrors;
